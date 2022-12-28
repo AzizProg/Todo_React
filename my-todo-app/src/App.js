a126ad37394516e8210e79bd6c152fda802dcf0d
TRAORE Abdoul-Aziz <traoreabdoulaziz70@gmail.com>
2022-12-28 03:45:20 +0000

    form add todo

--- my-todo-app/src/App.js
+++ my-todo-app/src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const addTodo = () => {
+    if (todo !== "") {
+      setTodos([...todos, todo]);
+      setTodo("");
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+      <p className="text-3xl text-gray-700 font-bold mb-5">Welcome!</p>
+      <p className="text-gray-500 text-lg">React and Tailwind CSS in action</p>
+      <div className="">
+        <input
+          name="todo"
+          className="border-black "
+          placeholder="Ajouter une tâche"
+          value={todo}
+          onChange={(e) => {
+            setTodo(e.target.value);
+          }}
+        />
+        <button onClick={addTodo} className="bg-[green] block cursor p-2 m-2">
+          Ajoutter
+        </button>
+      </div>
+
+      {todos?.length > 0 ? (
+        <ul>
+          {todos.map((todo, index) => (
+            <div>
+              <li key={index}> {todo}</li>
+              <button className="bg-[red]">Delete</button>
+            </div>
+          ))}
+        </ul>
+        ) : (
+          <div className="empty">
+          <p>No task found</p>
+        </div>
+      )}
     </div>
   );
 }
